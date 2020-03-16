b339bc3e8e2c572f4dfae870ba7109e3510d1cc2
Ben Steadman <steadmanben1@gmail.com>
2020-03-16 20:12:34 +0000

    Improved error handling.
    This commit introduces a custom Error type to encapsulate the expected
    errors from parsing input to TIOCSTI IOCTL calls (NOTTY, EPERM, invalid
    CString e.t.c). This is then used in main() to provide explicit error
    messages to the user upon error. Furthermore, this lays some initial
    groundwork for pulling out the core functionality into a lib to be
    consumed programatically.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate libc;
 
 use libc::{c_char, TIOCSTI};
-use std::ffi::CString;
+use nix;
+use nix::errno::Errno;
+use nix::ioctl_write_ptr_bad;
+use std::ffi::{CString, NulError};
+use std::fmt;
 use std::fs::OpenOptions;
+use std::io;
 use std::os::unix::io::AsRawFd;
 use std::path::{Path, PathBuf};
-
-use nix::ioctl_write_ptr_bad;
 use structopt::StructOpt;
 
 ioctl_write_ptr_bad!(tiocsti_write, TIOCSTI, c_char);
@@ -25,32 +28,73 @@ struct Opt {
     no_newline: bool,
 }
 
-// TODO: Handle errors properly e.g. ENOTTY, EPERM etc
-fn termecho<P: AsRef<Path>>(devpath: &P, s: &CString) {
-    let dev = OpenOptions::new()
-        .read(false)
-        .write(true)
-        .open(devpath)
-        .expect("Error opening device");
+#[derive(Debug)]
+enum Error {
+    InvalidPermissions,
+    NoTTY,
+    IO(io::Error),
+    Nix(nix::Error),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Re-use debug for simplicity
+        write!(f, "{:?}", self)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Error {
+        Error::IO(e)
+    }
+}
+
+impl From<nix::Error> for Error {
+    fn from(e: nix::Error) -> Error {
+        match e {
+            nix::Error::Sys(Errno::EPERM) => Error::InvalidPermissions,
+            nix::Error::Sys(Errno::ENOTTY) => Error::NoTTY,
+            _ => Error::Nix(e),
+        }
+    }
+}
+
+// Default implementations are sufficient here
+impl std::error::Error for Error {}
+
+fn termecho<P: AsRef<Path>>(devpath: &P, s: &CString) -> Result<(), Error> {
+    let dev = OpenOptions::new().read(false).write(true).open(devpath)?;
 
     let fd = dev.as_raw_fd();
     let mut p = s.as_ptr();
 
     while unsafe { *p } != b'\0' as c_char {
         unsafe {
-            tiocsti_write(fd, p).unwrap();
+            tiocsti_write(fd, p)?;
             p = p.add(1);
         }
     }
     Ok(())
 }
 
+pub fn parse_cmd(cmd: Vec<String>, no_newline: bool) -> Result<CString, NulError> {
+    CString::new(cmd.join(" ") + if no_newline { "" } else { "\n" })
+}
+
 // TODO: Expose functionality as a library
 fn main() {
     let opt = Opt::from_args();
-    let s = CString::new(opt.cmd.join(" ") + if opt.no_newline { "" } else { "\n" })
-        .expect("cmd cannot be parsed to a valid CString");
+    let s = parse_cmd(opt.cmd, opt.no_newline).expect("cmd cannot be parsed to a valid CString");
     for devpath in opt.devices {
-        termecho(&devpath, &s);
+        let devpath_str = format!("{}", devpath.display());
+        match termecho(&devpath, &s) {
+            Err(Error::IO(_)) => eprintln!("Error opening {}", devpath_str),
+            Err(Error::InvalidPermissions) => {
+                eprintln!("Cannot write to {} - are you root?", devpath_str)
+            }
+            Err(Error::NoTTY) => eprintln!("{} is not a TTY/PTY", devpath_str),
+            Err(Error::Nix(e)) => eprintln!("Unable to write to {}: {:?}", devpath_str, e),
+            Ok(_) => println!("{} OK", devpath_str),
+        };
     }
 }
